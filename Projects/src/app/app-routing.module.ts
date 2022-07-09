753f3feeb34cb0f9ff6c97d82affc16083c7b823
labandia <65441114+labandia@users.noreply.github.com>
2022-07-09 23:27:38 +0800

    add a new project

---
+++ Projects/src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { TodolistComponent } from './todolist/todolist.component';
+
+const routes: Routes = [
+  {
+    path: 'todolist',
+    component: TodolistComponent,
+  },
+  {
+    path: '',
+    redirectTo: 'todolist',
+    pathMatch: 'full',
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
